3b11e1a4c0334e49af4d2ce06d47c5d914ebe56d
Charminha <83584290+Charminha@users.noreply.github.com>
2023-10-25 20:09:53 +0200

    Add score keeping to playRound function

--- javascript.js
+++ javascript.js
@@ -13,40 +13,50 @@ function playRound(playerSelection, computerSelection){
         if (playerSelection == "rock"){
             if (computerSelection == "paper"){
                 console.log("Paper beats rock! You lose!");
+                computerScore += 1;
             }
             else {
                 console.log("Rock beats scissors! You win!");
+                playerScore += 1;
             }
         }
         //if player choses paper
         if (playerSelection == "paper"){
             if (computerSelection == "scissors"){
                 console.log("Scissors beat paper! You lose!");
+                computerScore += 1;
             }
             else {
                 console.log("Paper beats rock! You win!");
+                playerScore += 1;
             }
         }
         //if player choses scissors
         if (playerSelection == "scissors"){
             if (computerSelection == "rock"){
                 console.log("Rock beats scissors! You lose!");
+                computerScore += 1;
             }
             else {
                 console.log("Scissors beat paper! You win!");
+                playerScore += 1;
             }
         }
     }
     else {
         console.log("That's a tie! Try again!");
     }
+    
 }
 
-/*function game(){
+/* function game(){
     let playerScore = 0;
     let computerScore = 0;
-    for (i=0; i<5; i++){
-        playRound(playerSelection, computerSelection);
+    playRound(playerSelection, computerSelection);
+        for (i=0; i<5; i++){
+            const playerSelection = "rock";
+            const computerSelection = getComputerChoice();
+            playRound(playerSelection, computerSelection);
     }
 }*/
 
