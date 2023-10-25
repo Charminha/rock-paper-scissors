903360325a0b73318948c3564bc8b5f97a3b7633
Charminha <83584290+Charminha@users.noreply.github.com>
2023-10-25 19:52:04 +0200

    Create playRound function with basic conditional logic
    The playRound function currently only displays the result on the console and returns nothing

--- javascript.js
+++ javascript.js
@@ -8,7 +8,38 @@ function getComputerChoice(){
 }
 
 function playRound(playerSelection, computerSelection){
-
+    //if player choses rock
+    if (playerSelection != computerSelection){
+        if (playerSelection == "rock"){
+            if (computerSelection == "paper"){
+                console.log("Paper beats rock! You lose!");
+            }
+            else {
+                console.log("Rock beats scissors! You win!");
+            }
+        }
+        //if player choses paper
+        if (playerSelection == "paper"){
+            if (computerSelection == "scissors"){
+                console.log("Scissors beat paper! You lose!");
+            }
+            else {
+                console.log("Paper beats rock! You win!");
+            }
+        }
+        //if player choses scissors
+        if (playerSelection == "scissors"){
+            if (computerSelection == "rock"){
+                console.log("Rock beats scissors! You lose!");
+            }
+            else {
+                console.log("Scissors beat paper! You win!");
+            }
+        }
+    }
+    else {
+        console.log("That's a tie! Try again!");
+    }
 }
 
 getComputerChoice();
