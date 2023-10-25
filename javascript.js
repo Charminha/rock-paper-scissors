410e7044b3abb8d3e85d558ef2d1eab01564388f
Charminha <83584290+Charminha@users.noreply.github.com>
2023-10-25 19:26:21 +0200

    Remove console.log() that was there for debugging

--- javascript.js
+++ javascript.js
@@ -4,7 +4,6 @@ function getComputerChoice(){
     const possibleChoices = ["rock", "paper", "scissors"];
     const random = Math.floor(Math.random() * possibleChoices.length);
     const computerSelection = possibleChoices[random];
-    console.log(computerSelection);
     return computerSelection;
 }
 
@@ -13,4 +12,3 @@ function playRound(playerSelection, computerSelection){
 }
 
 getComputerChoice();
-console.log("hello");
